ca853a06253b484ccaa24e2c9b12da4eee3c945d
Leroi Herisson <jonathan.ibarlucea@gmail.com>
2022-01-11 14:23:55 +0100

    x

--- client_insert.php
+++ client_insert.php
@@ -24,3 +24,54 @@ try {
 // SELECT * FROM etat;
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter Client</title>
+</head>
+
+
+<body>
+    <?php
+        
+        mysql_connect("localhost","root","root");
+        mysql_select_db("eCommercePHP");
+        $select="eCommercePHP";
+        if(isset($select)&&$select!=""){
+        $select=$_POST['NEW'];
+        }
+    ?>
+
+
+<form id="form_insert" action="" method="post">
+	<label for="nom">Nom :</label>
+	<input type="text" id="nom" name="nom" required>
+    <br>
+    <label for="email">Email :</label>
+	<input type="email" id="email" name="email" required>
+    <br>
+    <label for="telephone">Telphone :</label>
+	<input type="tel" id="telephone" name="telephone" required>
+    <br>
+    <label for="adresse">Adresse :</label>
+        <select name='adresse'>
+        <option value="">--- Select ---</option>
+        <?php
+        $stmt=$dbh->prepare("SELECT adresse FROM adresse");
+        $stmt->execute();
+        foreach($stmt->fetchAll() AS $line){
+          echo '<option value ="'.$line[0].'">'.$line[0].'</option>';
+        }
+
+        ?>
+        </select>
+        </form>
+    <br>
+	<button type="submit" form="form_insert" value="Submit">Valider</button>
+</form>
+
+</body>
+</html>

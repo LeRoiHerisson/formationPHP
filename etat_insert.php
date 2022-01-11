eb1ff907c27d36438049fa94921e756875ed67f4
Leroi Herisson <jonathan.ibarlucea@gmail.com>
2022-01-11 09:40:51 +0100

    ok

--- etat_insert.php
+++ etat_insert.php
@@ -1,7 +1,32 @@
 <?php
-require_once("config.php");
+//les 3 lignes permettent d'afficher les erreurs sur la page
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 
-$stmt = $bdh->prepare("INSERT INTO etat (libelle) VALUES (:libelle);");
+$dbh = new PDO('mysql:host=localhost;dbname=eCommercePHP','root','root');
 
+// pull all the clients limit to 25
+
+$stmt=$dbh->prepare("SELECT * FROM etats
+-- WHERE idclient = :idclient");
+
+$stmt->bindParam(':idetat', $_GET['idetat']);
+//pull php code
+
+$stmt->execute();
+
+// format this into html
 
 ?>
+<html>
+<head>
+<title>Insertion de etat dans la base</title>
+</head>
+<body></body>
+
+// lancement de la requete
+// $sql = 'INSERT INTO etat (libelle) VALUES (:libelle);"
+
+
+</html>
